6bec8c2ef08c3850717e2311dde76423ec22c65f
Sambiase <marsamb@gmail.com>
2023-02-28 15:57:34 -0300

    new files added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import cassandra
 from cassandra.cluster import Cluster
 
@@ -14,10 +16,12 @@ def list_all_keyspaces(session):
         print(rows.keyspace_name)
 
 
-def create_keyspace(session):
-    session.execute("CREATE KEYSPACE [IF NOT EXISTS] football_teams "
-                    "WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};")
-
+def create_keyspace(session, keyspace_name):
+    try:
+        session.execute(f"CREATE KEYSPACE [IF NOT EXISTS] {keyspace_name} "
+                        "WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};")
+    except cassandra.AlreadyExists as e:
+        print(f'[Erro] {e}')
 
 def use_keyspace(session,keyspace):
     try:
@@ -32,25 +36,52 @@ def create_table(session, table_name):
     except cassandra.AlreadyExists as e:
         print(f'[Erro] {e}')
 
+
+def list_all_tables(session):
+    rows = session.execute('DESC TABLES')
+    for row in rows:
+        print(row.name)
+
+
+def list_a_table(session, keyspace_name, table_name):
+    try:
+        session.execute(f"DESC {keyspace_name}.{table_name}")
+    except cassandra.InvalidRequest as e:
+        print(f'[Erro] {e}')
+
+
 if __name__ == '__main__':
     session = cassandra_connection()
     while True:
         print('\n---| Cassandra DB |---\n')
         print('[1] - List All Keyspaces')
-        print('[2] - Create Keyspace')
-        print('[3] - Use Keyspace')
+        print('[2] - List All Tables')
+        print('[3] - List one Table')
+        print('[4] - Create Keyspace')
+        print('[5] - Use Keyspace')
         print('[4] - Create Table')
         print('[5] - Insert Data Into Table')
-        print('[6] - List Table\n')
+        print('[6] - List Table')
+        print('[7] - Exit\n')
         res = int(input('Choice: '))
 
         if res == 1:
             list_all_keyspaces(session)
         elif res == 2:
-            create_keyspace(session)
+            list_all_tables(session)
         elif res == 3:
-            res_keyspace = str(input('Type Keyspace Name: '))
-            use_keyspace(session,res_keyspace)
+            keyspace_name = str(input('Keyspace Name: '))
+            table_name=str(input('Table Name: '))
+            list_a_table(session,keyspace_name,table_name)
         elif res == 4:
+            keyspace_name = str(input('Keyspace Name: '))
+            create_keyspace(session,keyspace_name)
+        elif res == 5:
+            keyspace_name = str(input('Keyspace Name: '))
+            use_keyspace(session, keyspace_name)
+        elif res == 6:
             table_name = str(input('Table Name: '))
             create_table(session,table_name)
+        elif res == 7:
+            print('\nGoodbye :)')
+            sys.exit(0)
